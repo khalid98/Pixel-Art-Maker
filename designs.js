ac233d69fd23c13a428bd4cc6aee7a4de9595510
khalid98 <33879750+khalid98@users.noreply.github.com>
2019-10-24 14:59:18 +0200

    First commit

---
+++ designs.js
@@ -0,0 +1,55 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+  // Your code goes here!
+ var table = document.getElementById('pixelCanvas');
+    table.innerHTML="";
+ var Height =document.getElementById('inputHeight').value;
+ var Width = document.getElementById('inputWidth').value;
+    var tabledata='';
+    for(var i=0;i<Height;i++){
+        tabledata+='<tr>';
+        for (var x=0;x<Width;x++){
+            tabledata+='<td></td>';
+        }
+        tabledata+='</tr>';
+    }
+ 
+    table.innerHTML=tabledata;
+    
+    //alert(tabledata);
+AddEventForTD();
+}
+function AddEventForTD()
+{
+var tds=document.getElementsByTagName('td');
+for(var i=0;i<tds.length;i++)
+{
+tds[i].addEventListener("click",function(event){
+var currenttd=event.target;
+currenttd.style.backgroundColor=document.getElementById('colorPicker').value;
+})
+}
+}
+ document.addEventListener('DOMContentLoaded', function () {
+AddEventForTD();
+
+})                              
+                                
+                                
+    
+
+
+
+
+
+
+
+
+
+
+
+
